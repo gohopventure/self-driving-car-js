6bcab35c844ccaf9bae274b85adaf974db4f052e
Daniel Hopson <hdhopson88@yahoo.com>
2022-07-29 20:45:54 -0500

    Adds collision detection for traffic

--- controls.js
+++ controls.js
@@ -1,11 +1,17 @@
 class Controls {
-    constructor() {
+    constructor(type) {
         this.forward = false;
         this.left = false;
         this.right = false;
         this.reverse = false;
 
-        this.#addKeyboardListeners();
+        switch(type) {
+            case "Keys":
+                this.#addKeyboardListeners();
+                break;
+            case "Dummy":
+                this.forward = true;
+        }
     }
 
     #addKeyboardListeners() {
